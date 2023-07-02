f2d97dccbcd177d09b6a90f908098c721804508c
Thabiso-007 <thabiso.hlatshwayo24@gmail.com>
2023-07-02 12:43:34 +0200

    Added files

---
+++ models/cart-model/cartModel.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose') // Erase if already required
+
+// Declare the Schema of the Mongo model
+var cartSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    productId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product"
+    },
+    quantity: {
+        type: Number,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    color: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Color"
+    }
+}, { timestamps: true })
+
+//Export the model
+const Cart = mongoose.model('Cart', cartSchema)
+module.exports = Cart
