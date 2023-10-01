0ce211df782eb4fb004df82d3200995d62676376
Thabiso-007 <thabiso.hlatshwayo24@gmail.com>
2023-10-01 15:51:08 +0200

    Modified

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 const { jwt_secret } = require('../config/env/index');
 
 const generateToken = (id) => {
-    return jwt.sign({ id }, jwt_secret, { expiresIn: '1d'});
+    return jwt.sign({ id }, jwt_secret, { expiresIn: '1h'});
 }
 
 module.exports = { generateToken }
