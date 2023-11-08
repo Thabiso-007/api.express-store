90ec0ad6db5e75b5d6ea29e54f256236827b41a5
Thabiso-007 <thabiso.hlatshwayo24@gmail.com>
2023-11-08 10:11:23 +0200

    Changed token expiry days

--- utils/refreshToken.js
+++ utils/refreshToken.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 const { jwt_secret } = require('../config/env/index');
 
 const generateRefreshToken = (id) => {
-    return jwt.sign({ id }, jwt_secret, { expiresIn: '3d'});
+    return jwt.sign({ id }, jwt_secret, { expiresIn: '1d'});
 }
 
 module.exports = { generateRefreshToken }
